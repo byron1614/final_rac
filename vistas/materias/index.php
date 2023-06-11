71bba3489e5a9b2681c40ad75ae1ae23885a56d2
byron <gemechama11203jorge@gmail.com>
2023-06-10 22:34:44 -0600

    se modifico el archivo index

--- vistas/materias/index.php
+++ vistas/materias/index.php
@@ -6,8 +6,8 @@
             <form action="/practica9/controladores/productos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="producto_nombre">NOMBRE DE LA MATERIA</label>
-                        <input type="text" name="producto_nombre" id="producto_nombre" class="form-control">
+                        <label for="mate_nombre">NOMBRE DE LA MATERIA</label>
+                        <input type="text" name="mate_nombre" id="mate_nombre" class="form-control">
                     </div>
                 </div>
               
