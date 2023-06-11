5cfb81c249a6820afa5562eda5bb63ca46a81595
byron <gemechama11203jorge@gmail.com>
2023-06-11 00:52:45 -0600

    modificacion del codigo buscar

--- vistas/notas/buscar.php
+++ vistas/notas/buscar.php
@@ -1,13 +1,12 @@
 <?php
-require_once '../../modelos/Cliente.php';
-require_once '../../modelos/Producto.php';
+require_once '../../modelos/Alumnos.php';
+require_once '../../modelos/Materias.php';
     try {
-        $cliente = new Cliente();
-        $producto = new Producto();
-        $clientes = $cliente->buscar();
-        $productos = $producto->buscar();
-            // var_dump($clientes);
-            // exit;
+        $alumno = new Alumno();
+        $materias = new Materia();
+        $alumnos = $alumno->buscar();
+        $materias = $materia->buscar();
+            
     } catch (PDOException $e) {
         $error = $e->getMessage();
     } catch (Exception $e2){
@@ -17,27 +16,20 @@ require_once '../../modelos/Producto.php';
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Formulario de búsqueda de ventas</h1>
+        <h1 class="text-center">FORMULARIO PARA BUSQUEDA DE ALUMNOS</h1>
         <div class="row justify-content-center">
-            <form action="/practica9/controladores/ventas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+            <form action="/final_rac/controladores/notas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="venta_cliente">Cliente</label>
-                        <select name="venta_cliente" id="venta_cliente" class="form-control">
+                        <label for="res_alumno">ALUMNO</label>
+                        <select name="res_alumno" id="res_alumno" class="form-control">
                             <option value="">SELECCIONE...</option>
-                            <?php foreach ($clientes as $key => $cliente) : ?>
-                                <option value="<?= $cliente['CLIENTE_ID'] ?>"><?= $cliente['CLIENTE_NOMBRE'] ?></option>
+                            <?php foreach ($alumnos as $key => $alumno) : ?>
+                                <option value="<?= $alumno['ALUM_ID'] ?>"><?= $alumno['ALUM_NOMBRE'] . ' ' . $alumno['ALUM_APELLIDO'] ?></option>
                             <?php endforeach?>
                         </select>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="venta_fecha">Fecha de la venta</label>
-                        <input type="datetime-local" value="<?= date('Y-m-d H:i') ?>" name="venta_fecha" id="venta_fecha" class="form-control">
-                    </div>
-                </div>
-               
                 <div class="row mb-3">
                     <div class="col">
                         <button type="submit" class="btn btn-info w-100">Buscar</button>
