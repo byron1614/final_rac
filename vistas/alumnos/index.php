c1e5112b38258bc3a5160ff4110fafd3c19d661b
byron <gemechama11203jorge@gmail.com>
2023-06-10 20:27:56 -0600

    moficacion del codigo index

--- vistas/alumnos/index.php
+++ vistas/alumnos/index.php
@@ -3,35 +3,35 @@
     <div class="container">
         <h1 class="text-center">FORMULARIO DE INGRESO DE ALUMNOS</h1>
         <div class="row justify-content-center">
-            <form action="/practica9/controladores/clientes/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/final_rac/controladores/alumnos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                    <label for="cliente_nombre">NOMBRE DEL ALUMNO</label>
-                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" required>
+                    <label for="alum_nombre">NOMBRE DEL ALUMNO</label>
+                        <input type="text" name="alum_nombre" id="alum_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nit">APELLIDO DEL ALUMNO</label>
-                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control" required>
+                        <label for="alum_apellido">APELLIDO DEL ALUMNO</label>
+                        <input type="text" name="alum_apellido" id="alum_apellido" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nit">GRADO</label>
-                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control" required>
+                        <label for="alum_grado">GRADO</label>
+                        <input type="text" name="alum_grado" id="alum_grado" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nit">ARMA</label>
-                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control" required>
+                        <label for="alum_arma">ARMA</label>
+                        <input type="text" name="alum_arma" id="alum_arma" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nit">NACIONALIDAD</label>
-                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control" required>
+                        <label for="alum_nacionalidad">NACIONALIDAD</label>
+                        <input type="text" name="alum_nacionalidad" id="alum_nacionalidad" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
