b0449dcf5baa814ab48dfce4faf193067475ef30
byron <gemechama11203jorge@gmail.com>
2023-06-10 19:01:54 -0600

    modificacion del formulario buscar alumno

--- vistas/alumnos/buscar.php
+++ vistas/alumnos/buscar.php
@@ -1,18 +1,24 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Buscar clientes</h1>
+        <h1 class="text-center">BUSCAR ALUMNOS</h1>
         <div class="row justify-content-center">
             <form action="/practica9/controladores/clientes/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nombre">Nombre del cliente</label>
+                        <label for="cliente_nombre">NOMBRE DEL ALUMNO</label>
                         <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="cliente_nit">Nit del cliente</label>
+                        <label for="cliente_nit">APELLIDO DEL ALUMNO</label>
+                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="cliente_nit">NACIONALIDAD</label>
                         <input type="text" name="cliente_nit" id="cliente_nit" class="form-control">
                     </div>
                 </div>
