625c756182936a57f6055003b76c62794e0be65e
byron <gemechama11203jorge@gmail.com>
2023-06-11 18:26:03 -0600

    modificacion del codigo modificar

--- controladores/alumnos/modificar.php
+++ controladores/alumnos/modificar.php
@@ -2,14 +2,15 @@
 require '../../modelos/Alumnos.php';
 
 
-if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_grado'] != '' && $_POST['alum_arma'] != '' && $_POST['alum_nacionalidad'] != ''){
+if($_POST['alum_id'] != '' && $_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_grado'] != '' && $_POST['alum_arma'] != '' && $_POST['alum_nacionalidad'] != ''){
 
     try {
         $alumno = new Alumno($_POST);
-        $resultado = $alumno->guardar();
-        $error = "NO se guardó correctamente";
+        $resultado = $alumno->modificar();
+
     } catch (PDOException $e) {
         $error = $e->getMessage();
+
     } catch (Exception $e2){
         $error = $e2->getMessage();
     }
@@ -25,7 +26,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Resultados</title>
+    <title>Resultados alumnos</title>
 </head>
 <body>
     <div class="container">
@@ -33,7 +34,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
             <div class="col-lg-6">
                 <?php if($resultado): ?>
                     <div class="alert alert-success" role="alert">
-                        Guardado exitosamente!
+                        Modificado exitosamente!
                     </div>
                 <?php else :?>
                     <div class="alert alert-danger" role="alert">
@@ -45,7 +46,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <a href="/final_rac/vistas/alumnos/index.php" class="btn btn-info">Volver al formulario</a>
+                <a href="/final_rac/controladores/alumnos/buscar.php?alum_nombre=<?= $_POST['alum_nombre'] ?>" class="btn btn-info">Volver al formulario</a>
             </div>
         </div>
     </div>
