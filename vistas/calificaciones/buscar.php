598b859cdf9070603339330d854ae6152f255b0f
byron <gemechama11203jorge@gmail.com>
2023-06-11 20:16:04 -0600

    cambios realizados

--- vistas/calificaciones/buscar.php
+++ vistas/calificaciones/buscar.php
@@ -15,12 +15,12 @@ require_once '../../modelos/Materias.php';
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Formulario de búsqueda de calificaciones</h1>
+        <h1 class="text-center">FORMULARIO DE BUSQUEDA DE CALIFICACIONES</h1>
         <div class="row justify-content-center">
             <form action="/final_rac/controladores/calificaciones/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="res_alumno">Alumno</label>
+                        <label for="res_alumno">NOMBRE DEL ALUMNO</label>
                         <select name="res_alumno" id="res_alumno" class="form-control">
                             <option value="">SELECCIONE...</option>
                             <?php foreach ($alumnos as $key => $alumno) : ?>
