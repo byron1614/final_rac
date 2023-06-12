c70263472580ffdb2ac074ac8d114f7d3b4c3b9a
byron <gemechama11203jorge@gmail.com>
2023-06-11 20:31:52 -0600

    cambios en el codigo

--- vistas/calificaciones/rel_mate_alum.php
+++ vistas/calificaciones/rel_mate_alum.php
@@ -2,31 +2,27 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require '../../modelos/Resultados.php';  
-require '../../modelos/Rel_mate_alum.php';  
+require '../../modelos/Calificaciones.php';
+require '../../modelos/Rel_mate_alum.php';
+    try {
+        $id = $_GET['res_id'];
+        $calificacion = new Calificacion($_GET);
+        $Rel_mate_alum = new Rel_mate_alum([
+            'res_materia' => $id
+        ]);
 
-try {
-    $id = $_GET['res_id'];
-    $calificacion = new Calificacion([
-        'res_ID' => $id 
-    ]);
-    $relacionmatalum = new RelacionMatAlum([
-        'mate_materia' => $id  
-    ]);
-
-    $calificaciones = $calificacion->buscar();
-    $materias = $rel_mate_alum ->buscar();
-    echo "<pre>";
-    var_dump($calificaciones);
-    echo "</pre>";
-    echo "<pre>";
-    var_dump($materias);
-    echo "</pre>";
-    exit;
-    // $error = "NO se guardó correctamente";
-} catch (PDOException $e) {
-    $error = $e->getMessage();
-} catch (Exception $e2){
-    $error = $e2->getMessage();
-}
+        $calificaciones = $calificacion->buscar();
+        $materias = $Rel_mate_alum->buscar();
+        echo "<pre>";
+        var_dump($calificaciones);
+        echo "</pre>";
+        echo "<pre>";
+        var_dump($materias);
+        echo "</pre>";
+        exit;
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2){
+        $error = $e2->getMessage();
+    }
 ?>
