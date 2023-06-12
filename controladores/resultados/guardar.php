a1d9a661a77f4f951ea3d840606d807e7c3161da
byron <gemechama11203jorge@gmail.com>
2023-06-12 15:01:35 -0600

    cambios en los codigos

--- controladores/resultados/guardar.php
+++ controladores/resultados/guardar.php
@@ -1,39 +1,50 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once '../../modelos/Resultados.php';
 require_once '../../modelos/Rel_mate_alum.php';
 
-if($_POST['res_alumno'] != ''){
 
-    
-    try {
-        $resultado = new Resultado($_POST);
-        $resultado = $resultado->guardar();
-        $error = "NO se guardó correctamente";
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-    } catch (Exception $e2){
-        $error = $e2->getMessage();
+
+
+  
+
+if(isset($_POST)){
+
+    if($_POST['res_alumno'] != '' && $_POST['res_materia'] != '' && $_POST['res_punteo'] != ''){
+
+        $res_alumno = $_POST['res_alumno'];
+        $res_materia = $_POST['res_materia'];
+        $res_punteo = $_POST['res_punteo'];
+        $res_resultado = nota_literal($res_punteo);
+
+        $arg = [
+            'res_alumno' => $res_alumno,
+            'res_materia' => $res_materia,
+            'res_punteo' => $res_punteo,
+            'res_resultado' => $res_resultado
+        ];
+        
+        try {
+            $resultado = new resultado($arg);
+            $resultado = $resultado->guardar();
+            $error = "NO se guardó correctamente";
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+        } catch (Exception $e2){
+            $error = $e2->getMessage();
+        }
+    }else{
+        $error = "Debe llenar todos los datos";
     }
+
 }else{
-    $error = "Debe llenar todos los datos";
+    $error = "No se recibieron datos";
 }
 
-
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Resultados</title>
-</head>
-<body>
-    <div class="container">
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+
+    <div class="container mt-5">
         <div class="row">
             <div class="col-lg-6">
                 <?php if($resultado): ?>
@@ -54,5 +65,4 @@ if($_POST['res_alumno'] != ''){
             </div>
         </div>
     </div>
-</body>
-</html>
+    <?php include_once '../../includes/footer.php'?>
