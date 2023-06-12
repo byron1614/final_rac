1bd612b9febffd76b3b27c67e884995cfa4fe060
byron <gemechama11203jorge@gmail.com>
2023-06-11 18:21:40 -0600

    codigo modificado

--- controladores/alumnos/guardar.php
+++ controladores/alumnos/guardar.php
@@ -9,7 +9,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
     try {
         $alumno = new Alumno($_POST);
         $resultado = $alumno->guardar();
-        $error = "No se guardó correctamente";
+        $error = "NO se guardó correctamente";
     } catch (PDOException $e) {
         $error = $e->getMessage();
     } catch (Exception $e2){
@@ -27,7 +27,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Resultados</title>
+    <title>Resultados alumnos</title>
 </head>
 <body>
     <div class="container">
@@ -47,7 +47,7 @@ if($_POST['alum_nombre'] != '' && $_POST['alum_apellido'] != '' && $_POST['alum_
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <a href="/final_rac/vistas/alumnos/index.php" class="btn btn-info">Volver al formulario</a>
+                <a href="/final_cornelio/vistas/alumnos/index.php" class="btn btn-info">Volver al formulario</a>
             </div>
         </div>
     </div>
