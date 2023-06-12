88c336088aa7176ac13907a544bf4bfaaced2399
byron <gemechama11203jorge@gmail.com>
2023-06-11 19:31:57 -0600

    modificacion del codigo

--- modelos/materias.php
+++ modelos/materias.php
@@ -1,47 +1,46 @@
 <?php
 require_once 'Conexion.php';
-
-class Materia extends Conexion{
-    public $mate_ID;
-    public $mate_Nombre;
-    public $detalle_situacion;
-
-    public function __construct($args = [] )
-    {
-        $this->mate_ID = $args['mate_ID'] ?? null;
-        $this->mate_Nombre = $args['mate_Nombre'] ?? '';
-        $this->detalle_situacion = $args['detalle_situacion'] ?? '1';
-    }
-
-    public function guardar(){
-        $sql = "INSERT INTO Materias(mate_Nombre) VALUES ('$this->mate_Nombre')";
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
-
-    public function buscar(){
-        $sql = "SELECT * FROM Materias WHERE detalle_situacion = '1'";
-
-        if($this->mate_ID != null){
-            $sql .= " AND mate_ID = $this->mate_ID";
+    
+    class Materia extends Conexion{
+        public $mate_id;
+        public $mate_nombre;
+        public $detalle_situacion;
+    
+        public function __construct($args = [] )
+        {
+            $this->mate_id = $args['mate_id'] ?? null;
+            $this->mate_nombre = $args['mate_nombre'] ?? '';
+            $this->detalle_situacion = $args['detalle_situacion'] ?? '1';
         }
-
-        $resultado = self::servir($sql);
-        return $resultado;
+    
+        public function guardar(){
+            $sql = "INSERT INTO materias(mate_nombre) VALUES ('$this->mate_nombre')";
+            $resultado = self::ejecutar($sql);
+            return $resultado;
+        }
+    
+        public function buscar(){
+            $sql = "SELECT * FROM materias WHERE detalle_situacion = 1";
+    
+            if($this->mate_nombre != null){
+                $sql .= " AND mate_nombre = '$this->mate_nombre'";
+            }
+    
+            $resultado = self::servir($sql);
+            return $resultado;
+        }
+    
+        public function modificar(){
+            $sql = "UPDATE materias SET mate_nombre = '$this->mate_nombre' WHERE mate_id = $this->mate_id";
+            
+            $resultado = self::ejecutar($sql);
+            return $resultado;
+        }
+    
+        public function eliminar(){
+            $sql = "UPDATE materias SET detalle_situacion = '0' WHERE mate_id = $this->mate_id";
+            
+            $resultado = self::ejecutar($sql);
+            return $resultado;
+        }
     }
-
-    public function modificar(){
-        $sql = "UPDATE Materias SET mate_Nombre = '$this->mate_Nombre' WHERE mate_ID = $this->mate_ID";
-        
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
-
-    public function eliminar(){
-        $sql = "UPDATE Materias SET detalle_situacion = '0' WHERE mate_ID = $this->mate_ID";
-        
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
-}
-?>
