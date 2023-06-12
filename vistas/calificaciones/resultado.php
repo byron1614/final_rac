38df48e0f38ed3c71dea7efcb84c62fcb2565adb
byron <gemechama11203jorge@gmail.com>
2023-06-11 22:51:34 -0600

    modificacion del codigo

--- vistas/calificaciones/resultado.php
+++ vistas/calificaciones/resultado.php
@@ -10,10 +10,10 @@ require '../../modelos/Alumnos.php';
         $id = $_GET['res_id'];
         $calificacion = new Calificacion();
 
-        $promedio = $calificacion->promedio($id);
+        $resultado = $calificacion->resultado($id);
 
  
-        $subpromedio = 0;
+        $subresultado = 0;
         $nota = 0;
     } catch (PDOException $e) {
         $error = $e->getMessage();
@@ -31,51 +31,50 @@ require '../../modelos/Alumnos.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>FACTURA</title>
+    <title>RESULTADOS</title>
 </head>
 <body>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-6 table-responsive">
             <table class="table table-bordered">
-            <thead>
-                <tr class="text-center table-dark">
-                    <th colspan="3">DETALLE DEL RESULTADO.</th>
-                </tr>
+    <thead>
+        <tr class="text-center table-dark">
+            <th colspan="3">DETALLE DEL RESULTADO.</th>
+        </tr>
+    </thead>
+    <tbody>
                <tr>
-            <td><strong>NOMBRE:</strong></td>
-            <?php foreach ($promedio as $key => $calificacion) : ?>
-                <td><?= $calificacion['ALUM_NOMBRE'] . ' ' . $calificacion['ALU_APELLIDO'] ?></td>
+            <td><strong>NOMBRE DEL ALUMNO:</strong></td>
+            <?php foreach ($resultado as $key => $calificacion) : ?>
+                <td><?= $calificacion['ALUM_NOMBRE'] . ' ' . $calificacion['ALUM_APELLIDO'] ?></td>
             <?php endforeach ?>
         </tr>
         <tr>
-            <td><strong>GRADO:</strong></td>
-            <?php foreach ($promedio as $key => $calificacion) : ?>
+            <td><strong>GRADO MILITAR DEL ALUMNO:</strong></td>
+            <?php foreach ($resultado as $key => $calificacion) : ?>
                 <td><?= $calificacion['ALUM_GRADO'] ?></td>
             <?php endforeach ?>
         </tr>
         <tr>
-            <td><strong>ARMA:</strong></td>
-            <?php foreach ($promedio as $key => $calificacion) : ?>
+            <td><strong>ARMA DEL ALUMNO:</strong></td>
+            <?php foreach ($resultado as $key => $calificacion) : ?>
                 <td><?= $calificacion['ALUM_ARMA'] ?></td>
             <?php endforeach ?>
         </tr>
         <tr>
             <td><strong>NACIONALIDAD:</strong></td>
-            <?php foreach ($promedio as $key => $calificacion) : ?>
-                <td><?= $calificacion['ALU_NAC'] ?></td>
+            <?php foreach ($resultado as $key => $calificacion) : ?>
+                <td><?= $calificacion['ALUM_NACIONALIDAD'] ?></td>
             <?php endforeach ?>
         </tr>
-        <?php if (count($promedio) == 0) : ?>
+        <?php if (count($resultado) == 0) : ?>
             <tr>
                 <td colspan="3">NO EXISTEN REGISTROS</td>
             </tr>
-        <?php endif ?>
-    </tbody>
-</table>
-            </div>
-        </div>
-
+         <?php endif ?>
+        </tbody>
+    </table>
         <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-6 table-responsive">
@@ -93,13 +92,13 @@ require '../../modelos/Alumnos.php';
                        </tr>
                     </thead>
                     <tbody>
-                        <?php if (count($promedio) > 0) : ?>
-                            <?php foreach ($promedio as $key => $calificacion) : ?>
+                        <?php if (count($resultado) > 0) : ?>
+                            <?php foreach ($resultado as $key => $calificacion) : ?>
                                 <tr>
                                     <td><?= $key + 1 ?></td>
-                                    <td><?= $calificacion ['MA_NOMBRE'] ?></td>
-                                    <td><?= $calificacion['CALIF_PUNTEO'] ?></td>
-                                    <td><?= $calificacion['CALIF_RESULTADO'] ?></td>                 
+                                    <td><?= $calificacion ['MATE_NOMBRE'] ?></td>
+                                    <td><?= $calificacion['RES_PUNTEO'] ?></td>
+                                    <td><?= $calificacion['RES_RESULTADO'] ?></td>                 
                                 </tr>
                             <?php endforeach ?>
                         <?php else : ?>
@@ -108,9 +107,9 @@ require '../../modelos/Alumnos.php';
                             </tr>
                         <?php endif ?>
                         <tr>
-                        <td colspan="4">PROMEDIO:</td>
-                        <td><?= $calificacion['PROMEDIO'] ?></td>
-                        </tr>
+                        <td colspan="4">RESULTADO:</td>
+                        <td><?= $calificacion['RESULTADO'] ?></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -118,7 +117,7 @@ require '../../modelos/Alumnos.php';
 
         <div class="row justify-content-center">
             <div class="col-lg-4">
-                <a href="/final_cornelio/vistas/calificaciones/buscar.php" class="btn btn-info w-100">Volver al formulario</a>
+                <a href="/final_rac/vistas/calificaciones/buscar.php" class="btn btn-info w-100">Volver al formulario</a>
             </div>
         </div>
     </div>
